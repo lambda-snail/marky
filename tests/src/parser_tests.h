70cd3ff7c75524b291d4b0965f5d43cef904c971
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-08-11 20:22:07 +0200

    add more tests for mixed paragraphs and headers case

--- tests/src/parser_tests.h
+++ tests/src/parser_tests.h
@@ -3,6 +3,7 @@
 #include <marky/parser.h>
 
 #include <gtest/gtest.h>
+#include <math.h>
 
 std::string header_one_level =
 R"(
@@ -27,16 +28,19 @@ This is another paragraph!
 class test_visitor : public marky::markdown_visitor<>
 {
 public:
-    int headers = 0;
-    int paragraphs = 0;
+    int headers = 0, level = 0;
+    int paragraphs = 0, words = 0;
     void visit_paragraph(marky::ast::paragraph const& p) override
     {
         ++paragraphs;
+        words += static_cast<int>(p.items.size());
     }
 
     void visit_header(marky::ast::header const& h) override
     {
         ++headers;
+        level += static_cast<int>(h.level.size());
+        words += static_cast<int>(h.items.size());
     }
 };
 
@@ -72,4 +76,15 @@ TEST_F(ParserTests, MixedHeadersParagraphs_ShouldParse)
     EXPECT_TRUE(r);
     EXPECT_EQ(2, v.paragraphs);
     EXPECT_EQ(2, v.headers);
+    EXPECT_EQ(3, v.level);
+}
+
+TEST_F(ParserTests, MixedHeadersParagraphs_WordsShouldNotDisappear)
+{
+    bool r = marky::parse_string(two_headers_two_paragraphs.begin(),
+                                 two_headers_two_paragraphs.end(),
+                                 &v);
+
+    EXPECT_TRUE(r);
+    EXPECT_EQ(30, v.words);
 }
